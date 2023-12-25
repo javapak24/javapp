815d7292b42b9b934187035c05f194cadca85290
javapak24 <ajava2424@gmail.com>
2023-12-24 21:47:06 -0500

    updated scripts.js for task 1.2 on /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello There!');
-let favFood = 'ice cream';
-document.write(favFood);
+// height is in m
+
+let pokemonList = [
+    {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['fire', 'flying']
+    },
+    {
+        name: 'Squirtle',
+        height: .5,
+        type: ['water']
+    },
+    {
+        name: 'Pikachu',
+        height: .4,
+        type: ['electric']
+    }
+];
